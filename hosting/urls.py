973a91912e83111cb8fce73a96e269b08654daa3
Diane Corriette <131492009+todiane@users.noreply.github.com>
2026-02-01 20:41:59 +0000

    Update success template

--- hosting/urls.py
+++ hosting/urls.py
@@ -7,6 +7,7 @@ Include in your main urls.py:
 
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 app_name = "hosting"
 
@@ -14,4 +15,9 @@ urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("signup/success/", views.signup_success, name="signup_success"),
     path("signup/cancelled/", views.signup_cancelled, name="signup-cancelled"),
+    # Preview
+    path(
+        "signup-preview/",
+        TemplateView.as_view(template_name="hosting/signup_success.html"),
+    ),
 ]
